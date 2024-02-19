a2a6ebeb7d04e04b266e5e382c40d9117972549d
Mark <strategy155@gmail.com>
2024-02-19 20:26:04 +0100

    finalized test for now

--- index.test.ts
+++ index.test.ts
@@ -1,11 +1,10 @@
 import { expect, test, describe } from "bun:test";
 import { server } from "./index";
 
-console.log(server)
-
 describe("server", () => {
   test("server startup", 
     () => {
+      server
     }
   )
-}
+})
