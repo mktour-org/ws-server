e09b2ad5f8c18aaf6a3416bb34d937bc2047d57b
Mark <strategy155@gmail.com>
2024-02-22 21:01:18 +0100

    added tests

--- lib/socket.test.ts
+++ lib/socket.test.ts
@@ -1,5 +1,7 @@
-import { expect, test, describe } from "bun:test";
+import { expect, test, describe, beforeAll} from "bun:test";
 import { SERVER_OPTIONS } from "./socket";
+
+
 
 describe("server", () => {
   test("server startup", 
@@ -7,7 +9,20 @@ describe("server", () => {
       const server = Bun.serve(SERVER_OPTIONS);
       expect(server.hostname).toBe("localhost");
       expect(server.port).toBe(8080);
+      server.stop();
+    }
+  )
+})
 
+
+describe("server connection", () => {
+  beforeAll(() => {
+    const server = Bun.serve(SERVER_OPTIONS);
+  })
+  test("connect to server", 
+    () => {
+      const socket = new WebSocket(`ws://localhost:8080`);
+      console.log(socket);
     }
   )
 })
