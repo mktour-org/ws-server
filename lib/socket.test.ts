388ac718f2f012b8911b9ef2b3848918a9272bb9
Mark <strategy155@gmail.com>
2024-02-24 17:50:05 +0100

    some connection startup

--- lib/socket.test.ts
+++ lib/socket.test.ts
@@ -1,23 +1,15 @@
-import { expect, test, describe, beforeAll} from "bun:test";
+import { expect, test, describe, beforeAll, afterAll, beforeEach, afterEach} from "bun:test";
 import { SERVER_OPTIONS } from "./socket";
+import type { Server } from "bun";
+
+
 
 
 
 describe("server", () => {
-  test("server startup", 
-    () => {
-      const server = Bun.serve(SERVER_OPTIONS);
-      expect(server.hostname).toBe("localhost");
-      expect(server.port).toBe(8080);
-      server.stop();
-    }
-  )
-})
-
-
-describe("server connection", () => {
-  beforeAll(() => {
-    const server = Bun.serve(SERVER_OPTIONS);
+  let server: Server;
+  beforeEach(() => {
+    server = Bun.serve(SERVER_OPTIONS);
   })
   test("connect to server", 
     () => {
@@ -25,4 +17,7 @@ describe("server connection", () => {
       console.log(socket);
     }
   )
+  afterEach(() => {
+    server.stop(true)
+  })
 })
